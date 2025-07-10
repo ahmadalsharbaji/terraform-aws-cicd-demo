84c7132b472e827ce33af1239a8d88eb8d516834
ahmadalsharbaji <ahmed.fager@gmail.com>
2025-07-11 01:21:52 +0200

    Added Provider

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+# terraform/main.tf
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "cicd-demo-bucket-${random_id.bucket_id.hex}"
+  force_destroy = true
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
